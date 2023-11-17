0868b138f921a73cf0c283225de64a4a8de206a7
MaceGibson <mace_gibson@yahoo.com>
2023-11-17 18:23:55 -0500

    Stubbed out README

---
+++ src/makechange/CashRegister.java
@@ -0,0 +1,22 @@
+package makechange;
+
+import java.util.Scanner;
+
+public class CashRegister {
+
+	public static void main(String[] args) {
+		/*
+		 * The user is prompted asking for the price of the item
+		 */
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter the price of your puchase: ");
+		double userPrice = kb.nextDouble();
+		
+		/*
+		 * The user is then prompted asking how much money was tendered by the customer
+		 */
+		
+		
+	}
+
+}
