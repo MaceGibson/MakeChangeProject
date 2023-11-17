46d18aa4435d9f1f763b80964a4179f05d7179e6
MaceGibson <mace_gibson@yahoo.com>
2023-11-17 18:25:49 -0500

    fixed comment blocks to comment lines for user stories

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -5,18 +5,15 @@ import java.util.Scanner;
 public class CashRegister {
 
 	public static void main(String[] args) {
-		/*
-		 * The user is prompted asking for the price of the item
-		 */
+		 // The user is prompted asking for the price of the item
 		Scanner kb = new Scanner(System.in);
-		System.out.println("Please enter the price of your puchase: ");
+		System.out.println("Please enter the price of the customer's puchase: ");
 		double userPrice = kb.nextDouble();
 		
-		/*
-		 * The user is then prompted asking how much money was tendered by the customer
-		 */
 		
+		 // The user is then prompted asking how much money was tendered by the customer
 		
+		System.out.println("How much money tendered by customer");
 	}
 
 }
