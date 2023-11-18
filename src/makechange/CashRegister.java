5997150595198121da36da065aa95167d3c6ace3
MaceGibson <mace_gibson@yahoo.com>
2023-11-17 19:04:45 -0500

    added the user stories 1-3 and started logic for method to calculate change

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -14,6 +14,30 @@ public class CashRegister {
 		 // The user is then prompted asking how much money was tendered by the customer
 		
 		System.out.println("How much money tendered by customer");
+		double customerTender = kb.nextDouble();
+		
+		//Display an appropriate message if the customer provided too little money or the exact amount
+		
+		if (userPrice > customerTender) {
+			System.out.println("This is not enough to pay for the purchase!!");
+		} else if (userPrice == customerTender) {
+			System.out.println("This is the exact amount due.");
+			System.out.println("Change due: $0.00");
+		} else {
+			double changeDue = calculateChange(userPrice, customerTender);
+			System.out.println("The change due to the customer is: " + changeDue);
+			
+		}
+	}
+	/*
+	 * If the amount tendered is more than the cost of the item,
+	 * display the number of bills and coins that should be given to the customer.
+	 */
+	
+	public static double calculateChange(double price, double tender) {
+		double change = tender - price;
+		
+		return change;
 	}
 
 }
